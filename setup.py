2073fd6bc34c47f13546acfc9cfc9147ccb4f7a8
Drew Monroe <d.monroe@snet.net>
2017-12-27 10:50:15 -0500

    PEP8 fixes, update README

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setup(name="autoldap",
       version="0.1",
       author="Russell Jancewicz",
       url="https://github.com/russjancewicz/autoldap",
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=["python-ldap==3.0.0b3",
+                        "future==0.16.0",
+                        "argparse==1.4.0"])
