0bf5e6d245008c780b14758f9f54e46aa4ec948e
Drew Monroe <d.monroe@snet.net>
2017-12-27 10:13:22 -0500

    Modularize repository, update gitignore, add setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name="autoldap",
+      version="0.1",
+      author="Russell Jancewicz",
+      url="https://github.com/russjancewicz/autoldap",
+      packages=find_packages())
